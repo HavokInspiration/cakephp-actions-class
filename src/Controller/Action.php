0d46f762e8b68decf02d38755e26500cb8dff244
Yves P <havokinspiration@gmail.com>
2017-07-17 22:34:52 +0200

    Reimplements the "invokeAction" method of the Action class

--- src/Controller/Action.php
+++ src/Controller/Action.php
@@ -8,7 +8,11 @@ abstract class Action extends Controller
 
     public function invokeAction()
     {
+        $request = $this->request;
+        /* @var callable $callable */
+        $callable = [$this, 'execute'];
 
+        return $callable(...array_values($request->getParam('pass')));
     }
 
     abstract function execute();
