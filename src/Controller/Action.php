a95dfe4edf37511eefe805d46768b3fa1a15edae
Yves P <havokinspiration@gmail.com>
2017-07-17 22:55:09 +0200

    Add docblocks to the Action class

--- src/Controller/Action.php
+++ src/Controller/Action.php
@@ -2,21 +2,54 @@
 namespace HavokInspiration\ActionsClass\Controller;
 
 use Cake\Controller\Controller;
+use LogicException;
 
+/**
+ * Class Action
+ *
+ * This is the base class your action classes should extend.
+ * Implementing a method `execute()` is the only requirement. The `execute()` method is the same thing as the action
+ * method you would use in a CakePHP Controller.
+ * Everything else will work as if you were using a CakePHP Controller.
+ */
 abstract class Action extends Controller
 {
 
-    protected $controllerName;
+    /**
+     * Controller name this action is binded to.
+     *
+     * @var string|null
+     */
+    protected $controllerName = null;
 
-    public function setControllerName($controllerName)
+    /**
+     * Returns the controller name this action is binded to.
+     *
+     * @return string|null Null if controllerName have not been resolved when the class was created.
+     */
+    public function getControllerName()
     {
-        $this->controllerName = $controllerName;
+        return $this->controllerName;
     }
 
     /**
-     * Get the viewPath based on controller name and request prefix.
+     * Sets the controller name this action is binded to.
      *
-     * @return string
+     * @param string $controllerName
+     * @return \HavokInspiration\ActionsClass\Controller\Action
+     */
+    public function setControllerName(string $controllerName) : Action
+    {
+        $this->controllerName = $controllerName;
+
+        return $this;
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * Override in order for the method to use the `controllerName` property of the object instead of `name` (which
+     * is used to store the action name) to append to the view path returned.
      */
     protected function _viewPath()
     {
@@ -32,14 +65,28 @@ abstract class Action extends Controller
         return $viewPath;
     }
 
+    /**
+     * {@inheritDoc}
+     *
+     * Rewrite it as we only expect a method 'execute' and not an action name that is auto-resolved.
+     */
     public function invokeAction()
     {
         $request = $this->request;
+        if (!isset($request)) {
+            throw new LogicException('No Request object configured. Cannot invoke action');
+        }
+
         /* @var callable $callable */
         $callable = [$this, 'execute'];
 
         return $callable(...array_values($request->getParam('pass')));
     }
 
+    /**
+     * Every action class should at least implement this method. This method will be called when the action is resolved.
+     *
+     * @return mixed The resulting response.
+     */
     abstract function execute();
 }
