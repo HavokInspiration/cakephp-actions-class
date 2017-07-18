b0a0b4a98612c7a11bbc1311e978fdcb9e7c4d83
Yves P <havokinspiration@gmail.com>
2017-07-18 22:37:34 +0200

    Prepare the repository to hold some unit tests

---
+++ tests/TestCase/Http/ActionFactoryTest.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Copyright (c) Yves Piquel (http://www.havokinspiration.fr)
+ *
+ * Licensed under The MIT License
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright (c) Yves Piquel (http://www.havokinspiration.fr)
+ * @link          http://github.com/HavokInspiration/cakephp-actions-class
+ * @license       http://www.opensource.org/licenses/mit-license.php MIT License
+ */
+namespace HavokInspiration\ActionsClass\Test\TestCase\Http;
+
+use Cake\TestSuite\TestCase;
+
+class ActionFactoryTest extends TestCase
+{
+
+    /**
+     * setUp.
+     *
+     * @return void
+     */
+    public function setUp()
+    {
+        parent::setUp();
+    }
+
+    /**
+     * tearDown.
+     *
+     * @return void
+     */
+    public function tearDown()
+    {
+        parent::tearDown();
+    }
+}
